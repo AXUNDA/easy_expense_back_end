864def69a8a30d7586f776802c4f2a738bbc4973
AXUNDA <charlesemmanuelazunda@gmail.com>
2023-09-26 15:40:47 +0100

    added database connection

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,11 @@ import routes from "./routes/index.routes";
 import { errorHandler } from "./middlewares/errorHandler";
 import { notFound } from "./middlewares/notFound";
 
+import dotenv from "dotenv";
+dotenv.config();
+
+import connect from "./db";
+
 const app = express();
 app.use(cors());
 app.use(morgan("dev"));
@@ -12,6 +17,7 @@ app.use("/", routes);
 app.use(errorHandler);
 app.use(notFound);
 
-app.listen(8080, () => {
+app.listen(8080, async () => {
+  await connect();
   console.log("listening");
 });
