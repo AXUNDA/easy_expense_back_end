fbd0e3dd44b5e4c9d29c27d80b0c3f45c417d183
AXUNDA <charlesemmanuelazunda@gmail.com>
2023-09-27 12:21:35 +0100

    fixes

--- build/models/Member.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const MemberSchema = new mongoose_1.Schema({
-    email: { type: String, required: true, unique: true },
-}, { timestamps: true });
-const Member = (0, mongoose_1.model)("Member", MemberSchema);
-exports.default = Member;
