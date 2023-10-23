57cb4829edf68f196379bf81d6cf955a00df12d1
AXUNDA <charlesemmanuelazunda@gmail.com>
2023-10-23 17:47:17 +0100

    root route

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.db as string);
+    await mongoose.connect(process.env.db2 as string);
     console.log("db connected");
   } catch (error) {
     console.log(error);
--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,4 +1,4 @@
-import express, { Router } from "express";
+import express, { Router, Request, Response } from "express";
 import authRoutes from "./auth.routes";
 import memberRoutes from "./member.routes";
 
@@ -7,5 +7,12 @@ const router: Router = express.Router();
 router.use("/auth", authRoutes);
 
 router.use("/members", memberRoutes);
+router.get("/", (req: Request, res: Response) => {
+  res.json({
+    status: "api  active",
+
+    rootRoute: true,
+  });
+});
 
 export default router;
